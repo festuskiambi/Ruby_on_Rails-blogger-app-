4601fb939f45d40318c9b7bb1191343ccd774746
kiamfestoh@gmail <kiamfestoh@gmail.com>
2015-04-21 18:39:55 +0300

    my completed app

--- db/migrate/20150414181850_create_comments.rb
+++ db/migrate/20150414181850_create_comments.rb
@@ -1,8 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def change
-  	
     create_table :comments do |t|
-      t.string :author_name
+      t.string :auther_name
       t.text :body
       t.references :article, index: true
 
